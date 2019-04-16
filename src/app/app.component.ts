3473867a0c5b09f6bcd0e80febe208abdf4121ff
Krystian <krystian.kat@gmail.com>
2019-04-16 21:39:36 +0200

    Main page template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,16 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpHeaders, HttpRequest} from '@angular/common/http';
+import {environment} from '../environments/environment';
+import {faSquareRootAlt} from '@fortawesome/free-solid-svg-icons';
+import {faHome} from '@fortawesome/free-solid-svg-icons';
+import {faCalculator} from '@fortawesome/free-solid-svg-icons';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import {faInfo} from '@fortawesome/free-solid-svg-icons/faInfo';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import {faCopyright} from '@fortawesome/free-solid-svg-icons';
+import {faHeart} from '@fortawesome/free-solid-svg-icons';
+import {faRocket} from '@fortawesome/free-solid-svg-icons/faRocket';
+import {faInfinity} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +18,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  square = faSquareRootAlt;
+  home = faHome;
+  calculator = faCalculator;
+  search = faSearch;
+  info = faInfo;
+  envelope = faEnvelope;
+  copyright = faCopyright;
+  heart = faHeart;
+  rocket = faRocket;
+  infinity = faInfinity;
   title = 'YourCalculatorFrontEnd';
+
+  constructor(private httpClient: HttpClient) {
+  }
+  onSend() {
+    const formula = {
+      expression: 'Math.pow((a+b+c),3)',
+      values: {
+        a: 2.0,
+        b: 3.9,
+        c: 4.56
+      }
+    };
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    };
+
+    const httpRequest = new HttpRequest('POST', (environment.baseUrl + '/api/v1/executor'), formula, httpOptions);
+    this.httpClient.request(httpRequest).subscribe(
+      (response) => {
+        console.log(response);
+      }
+    );
+  }
 }
