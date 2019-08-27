fef8c4fea0d8c839f268e7cd330b286427bf9d33
Krystian <krystian.kat@gmail.com>
2019-08-27 20:26:09 +0200

    Table for search prepared

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -5,8 +5,15 @@ import {ActivatedRoute} from "@angular/router";
 import {MatSort} from "@angular/material";
 import {MatTableDataSource} from "@angular/material";
 import {MatPaginator} from "@angular/material/paginator";
+import {faLink} from "@fortawesome/free-solid-svg-icons";
 
-
+export interface CalculatorTableInfo {
+  name: string;
+  owner: string;
+  description: string;
+  category: string;
+  key: string;
+}
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -15,21 +22,25 @@ import {MatPaginator} from "@angular/material/paginator";
 export class SearchComponent implements OnInit, OnDestroy, AfterViewInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-  displayedColumns: string[] = ['index', 'name', 'owner', 'desc'];
-  calculators: { key: string, data: CalculatorModel}[];
+  displayedColumns: string[] = ['index', 'name', 'owner', 'category', 'description', 'key', 'link'];
+  calculators: Array<CalculatorTableInfo>;
   dataSource;
+  link = faLink;
   constructor( private cs: CalculatorStorageService, private route: ActivatedRoute) {
 
   }
-
-  public doFilter = (value: string) => {
+  public doFilter = (value) => {
+    console.log(value);
     this.dataSource.filter = value.trim().toLocaleLowerCase();
   }
   ngOnInit() {
     const con: { key: string, data: CalculatorModel }[] = this.route.snapshot.data.calculators;
-    this.calculators = con;
+    this.calculators = con.map( item => {
+      return { name: item.data.information.name, owner: item.data.information.owner,
+        description: item.data.information.description, category: item.data.information.category, key: item.key};
+    })
+    console.log(this.calculators)
     this.dataSource = new MatTableDataSource(this.calculators);
-    console.log(this.dataSource);
   }
 
   ngOnDestroy(): void {
@@ -38,4 +49,5 @@ export class SearchComponent implements OnInit, OnDestroy, AfterViewInit {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
   }
+
 }
